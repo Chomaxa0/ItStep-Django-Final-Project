0562ce5ced4e87f58293af78866c5520891a6a38
Chomaxa0 <158461505+Chomaxa0@users.noreply.github.com>
2024-06-20 16:37:21 +0400

    Delete Project/voting_platform directory

--- Project/voting_platform/elections/user_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.user_candidate_list, name='user_candidate_list'),
-    path('vote/<int:pk>/', views.vote, name='vote'),
-]
-
--- Project/voting_platform/elections/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-import re
-
-def validate_alpha(value):
-    if not re.match(r'^[a-zA-Zა-ჰ]+$', value):  
-        raise ValidationError('Only English and Georgian alphabetic characters are allowed.')
